d068154bf2f07984f7ab132acede6fa5b4935246
RogerKSI <kittipong1996@hotmail.com>
2017-04-08 19:35:21 +0700

    add homepage

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,17 @@
+class StaticPagesController < ApplicationController
+  def index
+    @end_time = Time.new(2017,5,1)
+    @start_time = Time.now
+    @elapse = @end_time - @start_time
+    @day_left = (@elapse/60/60/24).floor
+    @elapse -= @day_left*60*60*24
+    
+    
+    @hour_left = (@elapse/60/60).floor
+    @elapse -= @hour_left*60*60
+
+    @min_left = (@elapse/60).floor
+    @elapse -= @min_left*60
+
+  end
+end
