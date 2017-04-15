290bc1b180f48c070984d18edd247cb91c8ba01d
RogerKSI <kittipong1996@hotmail.com>
2017-04-15 21:08:49 +0700

    fixed layout template for unsuccessful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,13 @@ class SessionsController < ApplicationController
 
   def create
     personnel = Personnel.find_by(email: params[:session][:email].downcase)
+    
     if personnel && personnel.authenticate(params[:session][:password])
       log_in personnel
-      params[:session][:remember_me] == '1' ? remember(personnel) : forget(personnel)
       redirect_back_or personnel
     else
       flash.now[:danger] = "Invalid email/password combination"
-      render 'new'
+      render 'new', layout: "layouts/free"
     end
     
   end
