992f6fc8720c92e8e71ab427853b3557d87d68d9
Kitipong <kittipong1996@hotmail.com>
2017-04-18 00:29:22 +0700

    add path for windowpdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -11,5 +11,5 @@
   WickedPdf.config = {
     #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
     #:layout => "pdf.html",
-    #:exe_path => '/usr/local/bin/wkhtmltopdf'
+    :exe_path => "#{ENV['WICKED_PATH']}"
   }
